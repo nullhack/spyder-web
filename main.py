7652591bc38dc7271252ed4cab69dcfbdbfd4d3a
nullhack <eric.oliveira.lopes@gmail.com>
2019-09-17 03:02:25 +0800

    adding async and task

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import hashlib
 import json
 from enum import Enum
 
+import time
+import asyncio
 
 class FilterType(str, Enum):
     filter_param = "param"
@@ -14,20 +16,24 @@ class FilterType(str, Enum):
 
 browser_dict = {}
 
-#browser.quit()
-
 app = FastAPI()
 
+async def p():
+    while True:
+        await asyncio.sleep(2)
+        print('RUNNING STILL')
+asyncio.create_task(p())
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 @app.get("/load")
-def load(name: str):
+async def load(name: str):
     return {'load':'code'}
 
 @app.get("/add_page")
-def add_page(url: str, expiry: int = 3600):
+async def add_page(url: str, expiry: int = 3600):
     h = hashlib.sha256(url.encode('utf-8')).hexdigest()[:16]
     if not h in browser_dict:
         options = Options()
@@ -38,30 +44,30 @@ def add_page(url: str, expiry: int = 3600):
     return h
 
 @app.get("/list_pages")
-def list_pages():
+async def list_pages():
     return {k:browser_dict[k]['url'] for k in browser_dict.keys()}
 
 @app.get("/{page_id}/get", response_class=HTMLResponse)
-def add_filter(page_id: str):
+async def add_filter(page_id: str):
     return browser_dict[page_id]['browser'].page_source
 
 @app.get("/{page_id}/add_filter")
-def add_filter(page_id: int, filter_str: str, filter_type: FilterType = 'param', frequency: int = None, expiry: int = None):
+async def add_filter(page_id: int, filter_str: str, filter_type: FilterType = 'param', frequency: int = None, expiry: int = None):
     return {'add_filter': settings}
 
 @app.get("/{page_id}/list_filters")
-def list_filters(page_id: int):
+async def list_filters(page_id: int):
     return {'list_filters': page_id}
 
 @app.get("/{page_id}/{filter_id}/get")
-def get_data(page_id: int, filter_id: int, start_time: int = None, end_time: int = None):
+async def get_data(page_id: int, filter_id: int, start_time: int = None, end_time: int = None):
     return {'get': page_id}
 
 @app.get("/{page_id}/{filter_id}/delete")
-def delete_data(page_id: int, filter_id: int, start_time: int, end_time: int = None):
+async def delete_data(page_id: int, filter_id: int, start_time: int, end_time: int = None):
     return {'delete_time': page_id}
 
 @app.get("/health")
-def health():
+async def health():
     return {'health': 'status'}
 
